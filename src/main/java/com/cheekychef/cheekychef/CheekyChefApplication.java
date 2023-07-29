8b6d0f07177f4bcd050d699b4f9a214cdba0e3e6
Sameer Singh <sameer.singh@WFX-LAP-933.local>
2023-07-30 01:58:47 +0530

    inital commit

---
+++ src/main/java/com/cheekychef/cheekychef/CheekyChefApplication.java
@@ -0,0 +1,13 @@
+package com.cheekychef.cheekychef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheekyChefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheekyChefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cheekychef/cheekychef/CheekyChefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheekychef.cheekychef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheekyChefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
