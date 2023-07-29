add734feac4d59fe14acfb3fdc8b93706fe2f8fb
Sameer Singh <sameer.singh@WFX-LAP-933.local>
2023-07-30 03:27:16 +0530

    inital-commit

---
+++ src/main/java/com/cheekychef/cheekychef/Controller/model/LogIn.java
@@ -0,0 +1,33 @@
+package com.cheekychef.cheekychef.Controller.model;
+
+public class LogIn {
+    String email;
+    String password;
+
+    public LogIn() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "LogIn{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
